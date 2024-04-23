3aedaaa3d631b34b601e48a9eb3ec12296bf884d
Lydia France <lfrance@turing.ac.uk>
2024-04-23 18:42:16 +0100

    Update importing and have tutorial notebook

--- src/birddmd/__init__.py
+++ src/birddmd/__init__.py
@@ -3,5 +3,7 @@ BirdDMD: Using DMD on bird wing data.
 """
 from __future__ import annotations
 
+from .ProcessData import replace_rot_xyz_name, get_flight_modes, subset_by, get_column_names
+from .DMDFigures import plot_markers_overDist, plot_2d_markers, plot_single_sequence
 __all__ = ("__version__",)
 __version__ = "0.1.0"
