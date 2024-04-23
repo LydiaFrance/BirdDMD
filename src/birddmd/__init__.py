2a4a57a67f3f26e43184a80b6ffc9d7b77ac2179
Lydia France <lfrance@turing.ac.uk>
2024-04-23 22:19:47 +0100

    Edit the init file

--- src/birddmd/__init__.py
+++ src/birddmd/__init__.py
@@ -5,5 +5,5 @@ from __future__ import annotations
 
 from .ProcessData import replace_rot_xyz_name, get_flight_modes, subset_by, get_column_names
 from .DMDFigures import plot_markers_overDist, plot_2d_markers, plot_single_sequence
-__all__ = ("__version__",)
+__all__ = ("__version__", "replace_rot_xyz_name", "get_flight_modes", "subset_by", "get_column_names", "plot_markers_overDist", "plot_2d_markers", "plot_single_sequence")
 __version__ = "0.1.0"
